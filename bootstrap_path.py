63c80b79cccdfe6335073771c7495afe1bc665d6
O'Connor <justinoconnor@uslnaloaner-m009.lan>
2020-12-30 20:50:24 -0500

    removed personal file

--- bootstrap_path.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.append('/Users/justinoconnor/Code/Github/architectures/architectures')
-sys.path.append('/Users/justinoconnor/Code/Github/architectures/architectures/azure')
-
-print(sys.path)
