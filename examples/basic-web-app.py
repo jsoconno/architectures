1668c9fbfd32f972ddd485a2da322539c229e449
O'Connor <jsoconno@gmail.com>
2021-01-09 13:56:04 -0500

    updated web app example based on updated providers

--- examples/basic-web-app.py
+++ examples/basic-web-app.py
@@ -2,36 +2,35 @@ from architectures.core import Graph, Cluster, Group, Node, Edge, Flow
 from architectures.themes import Clean, DarkMode
 
 from architectures.providers.azure.general import Internet
-from architectures.providers.azure.identity import AdDomainServices
-from architectures.providers.azure.network import DnsZones
-from architectures.providers.azure.web import AppServices, AppServiceEnvironments
-from architectures.providers.azure.storage import BlobStorage
-from architectures.providers.azure.database import SqlDatabases, SqlServers
-from architectures.providers.azure.compute import Vm
+from architectures.providers.azure.identity import AzureActiveDirectoryDomainServices
+from architectures.providers.azure.networking import DnsZonePublic
+from architectures.providers.azure.application import ApplicationService, ApplicationServiceEnvironment
+from architectures.providers.azure.storage import StorageAccountBlobCool
+from architectures.providers.azure.data import SqlDatabase, SqlServer
+from architectures.providers.azure.compute import VirtualMachine
 
 theme = DarkMode(graph_attr_overrides={"rankdir":"BT", "fontsize":"24"})
 
 with Graph('Web App Over SQL', theme=theme):
     with Group() as group:
-        azure_ad = AdDomainServices("Azure Active Directory")
+        azure_ad = AzureActiveDirectoryDomainServices("Azure Active Directory")
         internet = Internet("Internet")
-        dns = DnsZones("Azure DNS")
+        dns = DnsZonePublic("Azure DNS")
     with Cluster("Resource Group", style="dotted") as resource_group:
-        blob_storage = BlobStorage("Storage Blob")
+        blob_storage = StorageAccountBlobCool("Storage Blob")
         with Cluster("App Service Plan") as app_service_plan:
-            app_service = AppServices("App Service")
-            app_service_environment = AppServiceEnvironments("App Service Environment")
+            app_service = ApplicationService("App Service")
+            app_service_environment = ApplicationServiceEnvironment("App Service Environment")
 
         with Cluster("Azure SQL Database"):
-            sql_server = SqlServers("Logical Server")
-            sql_database = SqlDatabases("Database")
+            sql_server = SqlServer("Logical Server")
+            sql_database = SqlDatabase("Database")
 
             with Cluster("Another Cluster"):
                 with Cluster("Another Cluster"):
                     with Cluster("Another Cluster"):
-                        vm = Vm()
+                        vm = VirtualMachine()
 
     Edge(internet, [azure_ad, dns], style="dotted")
     Edge(internet, app_service)
-    #Edge(app_service, sql_server, style="invis")
     Edge(sql_server, sql_database)
