769456f6f357c6d64911f9e6a923fb02986f5c86
O'Connor <jsoconno@gmail.com>
2021-01-09 12:59:23 -0500

    added scripts folder and added some scripts for file cleanup

---
+++ scripts/convert.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import os
+from PIL import Image
+
+"""
+Random collection of some things used to clean up file names and icons.
+"""
+
+path = str(Path(__file__).parent.absolute())
+
+def rename_file(file_path):
+    os.rename(f, f.lower().replace('-', ' ').replace('(', '').replace(')', ''))
+
+def crop_png(png_path):
+    im = Image.open(f)
+    im.size  # (364, 471)
+    im.getbbox()  # (64, 89, 278, 267)
+    im2 = im.crop(im.getbbox())
+    im2.size  # (214, 178)
+    im2.save(png_path)
+ 
+for file in os.listdir(path):
+    f = f'{path}/{file}'
+    if '.png' in f:
+        crop_png(f)
