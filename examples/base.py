4f61fa2a0fb97bbf23a77cdc6232d47a131f36c7
O'Connor <jsoconno@gmail.com>
2021-01-11 00:16:08 -0500

    updated examples

--- examples/base.py
+++ examples/base.py
@@ -1,22 +1,42 @@
 from architectures.core import Graph, Cluster, Group, Node, Edge, Flow
-from architectures.themes import Default
+from architectures.themes import Default, LightMode
 
 theme = Default(graph_attr_overrides={"splines": "ortho"})
 
 with Graph("my architecture", theme=theme, show=True):
-    with Cluster("Container A") as container_a:
-        a = Node("END")
-        b = Node("START")
+    with Cluster("Cluster A") as cluster_a:
+        a = Node("A")
+        b = Node("B")
         c = Node("C")
 
-    with Cluster("Container B") as container_b:
+    with Cluster("Cluster B") as cluster_b:
         d = Node("D")
         e = Node("E")
 
-    with Cluster("Container C") as container_c:
-        f = Node("F")
-
-        with Cluster("Container D") as container_d:
+        with Cluster("Cluster C") as cluster_c:
+            f = Node("F")
             g = Node("G")
 
-    Flow([b, c, d, container_c, e, container_d, a])
+            with Cluster("Cluster D") as cluster_d:
+                h = Node("H")
+
+    # With Edges you can connect...
+
+    # Nodes to nodes
+    Edge(a, b)
+
+    # Nodes to clusters
+    Edge(c, cluster_b)
+
+    # Clusters to clusters
+    Edge(cluster_d, cluster_a, style="dotted")
+
+    # Lists of nodes or clusters to lists of nodes or clusters
+    Edge([d, f], [e, cluster_c])
+
+    # With Flows you can connect...
+
+    # Any list of nodes or clusters with support for nested lists
+    Flow([a, h, [b, g], c, f, d, e], color="red")
+
+    # All objects support keyword arguments to change the style
