7aec27d3f07c2c72d0753bf1f660ae4c0d9060de
O'Connor <jsoconno@gmail.com>
2021-01-11 17:46:27 -0500

    changes to example

--- examples/basic-web-app.py
+++ examples/basic-web-app.py
@@ -5,31 +5,31 @@ from architectures.providers.azure.general import Internet
 from architectures.providers.azure.identity import AzureActiveDirectoryDomainServices
 from architectures.providers.azure.networking import DnsZonePublic
 from architectures.providers.azure.application import ApplicationService, ApplicationServiceEnvironment
-from architectures.providers.azure.storage import StorageAccountBlobCool
+from architectures.providers.azure.storage import StorageAccountBlobHot
 from architectures.providers.azure.data import SqlDatabase, SqlServer
-from architectures.providers.azure.compute import VirtualMachine
+from architectures.providers.azure.compute import VirtualMachineWindows
 
-theme = DarkMode(graph_attr_overrides={"rankdir":"BT", "fontsize":"24"})
+theme = LightMode(graph_attr_overrides={"rankdir":"BT", "fontsize":"24"}, hue="blue")
 
 with Graph('Web App Over SQL', theme=theme):
     with Group() as group:
-        azure_ad = AzureActiveDirectoryDomainServices("Azure Active Directory")
-        internet = Internet("Internet")
-        dns = DnsZonePublic("Azure DNS")
-    with Cluster("Resource Group", style="dotted") as resource_group:
-        blob_storage = StorageAccountBlobCool("Storage Blob")
+        azure_ad = AzureActiveDirectoryDomainServices()
+        internet = Internet()
+        dns = DnsZonePublic()
+    with Cluster(style="dotted") as resource_group:
+        blob_storage = StorageAccountBlobHot()
         with Cluster("App Service Plan") as app_service_plan:
-            app_service = ApplicationService("App Service")
-            app_service_environment = ApplicationServiceEnvironment("App Service Environment")
+            app_service = ApplicationService()
+            app_service_environment = ApplicationServiceEnvironment()
 
-        with Cluster("Azure SQL Database"):
-            sql_server = SqlServer("Logical Server")
-            sql_database = SqlDatabase("Database")
+        with Cluster("Azure SQL Database") as data_subnet:
+            sql_server = SqlServer()
+            sql_database = SqlDatabase()
 
             with Cluster("Another Cluster"):
                 with Cluster("Another Cluster"):
                     with Cluster("Another Cluster"):
-                        vm = VirtualMachine()
+                        vm = VirtualMachineWindows()
 
     Edge(internet, [azure_ad, dns], style="dotted")
     Edge(internet, app_service)
