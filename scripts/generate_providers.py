bb44a36efce202dd94fbb6cb358bdbc40d1e6257
Marin Muso <mmuso000@citymail.cuny.edu>
2021-01-16 11:07:06 -0500

    formatting

--- scripts/generate_providers.py
+++ scripts/generate_providers.py
@@ -1,19 +1,18 @@
 import os
 from pathlib import Path
 
+heading_text = f'# Do not modify this file directly. It is auto-generated with Python.\n\n'
+
 def format_text(text):
     return text.replace("-", " ").title().replace(" ", "")
-
-heading = f'# Do not modify this file directly. It is auto-generated with Python.\n\n'
 
 root_dir = os.path.dirname(os.getcwd())
 icons_dir = os.path.join(root_dir, "icons")
 providers_dir = os.path.join(root_dir, "architectures", "providers")
 
-
 init_file = os.path.join(providers_dir, "__init__.py")
 with open(init_file, "w+") as f:
-    f.write(heading)
+    f.write(heading_text)
     f.write(f'from architectures.core import Node\n\n')
 
 providers = os.listdir(icons_dir)
@@ -28,18 +27,17 @@ for provider in providers:
 
     for subdir, dirs, files in os.walk(os.path.join(icons_dir, provider)):
         if files:
-        
             provider_dir = os.path.join(providers_dir, provider)
             if not os.path.exists(provider_dir):
                 os.mkdir(provider_dir)
 
             service_type = os.path.split(subdir)[1]
             service_type_fmt = format_text(service_type)
-            
+
             service_file = os.path.join(provider_dir, service_type) + ".py"
             with open(service_file, "w+") as f:
                 # Create service type class
-                f.write(heading)
+                f.write(heading_text)
                 f.write(f'from architectures.providers import _{provider_fmt}\n\n')
                 f.write(f'class _{service_type_fmt}(_{provider_fmt}):\n')
                 f.write(f'\t_service_type = \"{service_type}\"\n')
@@ -47,8 +45,7 @@ for provider in providers:
 
             for service in files:
                 raw_service = service.split(".")[0]
-                service_fmt = format_text(raw_service)
-                
+                service_fmt = format_text(raw_service)       
                 with open(service_file, 'a+', newline='') as f:
                     # Create service type class
                     f.write(f'class {service_fmt}(_{service_type_fmt}):\n')
