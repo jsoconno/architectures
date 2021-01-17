232f3d1b8e2da12cd8c62f526e8230eaa017edc2
O'Connor <jsoconno@gmail.com>
2021-01-17 00:38:30 -0500

    fixed image resolution

--- examples/step-2-1.py
+++ examples/step-2-1.py
@@ -10,21 +10,20 @@ from architectures.providers.azure.compute import VirtualMachineWindows
 from architectures.providers.azure.data import DataLake
 
 # Set the theme
-theme = DarkMode()
+theme = LightMode()
 
-with Graph("My Customized Theme", theme=theme):
+with Graph("Basic Architecture", theme=theme):
     with Cluster("Subscription"):
         with Cluster("Resource Group"):
             with Cluster("Virtual Network"):
                 with Cluster("App Gateway Subnet") as app_gateway_subnet:
                     app_gateway = ApplicationGateway()
-                
                 with Cluster("Build Subnet") as build_subnet:
                     load_balancer = LoadBalancer()
                     vm_1 = VirtualMachineWindows()
                     vm_2 = VirtualMachineWindows()
-                
-            data_lake = DataLake()
+                with Cluster("Data Tier Subnet") as data_tier_subnet:
+                    data_lake = DataLake()
 
     Flow([app_gateway_subnet, load_balancer, [vm_1, vm_2]])
     Edge(build_subnet, data_lake)
