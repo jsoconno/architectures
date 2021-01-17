b4852f835017d7bda5c5671656e79fc01ec7b3fd
O'Connor <jsoconno@gmail.com>
2021-01-17 02:43:15 -0500

    total overhaul of readme

--- examples/step-2-1.py
+++ examples/step-2-1.py
@@ -1,36 +1,11 @@
-# Import the base objects
-from architectures.core import Graph, Cluster, Group, Node, Edge, Flow
+The next important set of imports to know about are `themes`.  Themes are what give your graph the styling you want.  You can use what is available as is, pass in attribute overrides, or create entirely new themes to fit your needs.
+```
+from architectures.themes import Default, LightMode, DarkMode
+```
+If you are creating a diagram that uses provider services (such as AWS, GCP, or Azure), you will also need to import the services you want to use for those `providers`.
 
-# Import the available themes
-from architectures.themes import LightMode, DarkMode
-
-# Import provider services
-from architectures.providers.azure.networking import ApplicationGateway, LoadBalancer
-from architectures.providers.azure.compute import VirtualMachineWindows
-from architectures.providers.azure.data import DataLake
-
-# Set the theme and add overrides
-theme = LightMode(
-    graph_attr_overrides={"labeljust":"c", "nodesep":"0.5"}, 
-    cluster_attr_overrides={"style":"dotted"},
-    node_attr_overrides={"fontcolor":"dimgrey"}, 
-    edge_attr_overrides={"color":"dimgrey"},
-    color_overrides=["#EBF4FA" "#D7E9F5", "#C3DEEF", "#AFD3EA"]
-)
-
-with Graph("My Customized Theme", theme=theme):
-    with Cluster("Subscription"):
-        with Cluster("Resource Group"):
-            with Cluster("Virtual Network"):
-                with Cluster("App Gateway Subnet") as app_gateway_subnet:
-                    app_gateway = ApplicationGateway()
-                
-                with Cluster("Build Subnet") as build_subnet:
-                    load_balancer = LoadBalancer()
-                    vm_1 = VirtualMachineWindows()
-                    vm_2 = VirtualMachineWindows()
-                
-            data_lake = DataLake()
-
-    Flow([app_gateway_subnet, load_balancer, [vm_1, vm_2]])
-    Edge(build_subnet, data_lake)
+Here is an example that import an Azure Virtual Machine:
+```
+from architectures.providers.azure.compute import VirtualMachine
+```
+Importing this class will allow you to draw an Azure Virtual Machine on the graph.
