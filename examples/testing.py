b3a2471df22272928b9699d8d8d23f366343fac8
O'Connor <jsoconno@gmail.com>
2021-01-17 14:14:39 -0500

    updated testing file (not a real change)

--- examples/testing.py
+++ examples/testing.py
@@ -2,7 +2,7 @@
 from architectures.core import Graph, Cluster, Group, Node, Edge, Flow
 
 # Import the available themes
-from architectures.themes import LightMode, DarkMode
+from architectures.themes import Default, LightMode, DarkMode
 
 # Import provider services
 from architectures.providers.azure.networking import ApplicationGateway, LoadBalancer
@@ -10,7 +10,7 @@ from architectures.providers.azure.compute import VirtualMachineWindows
 from architectures.providers.azure.data import DataLake
 
 # Set the theme
-theme = LightMode()
+theme = Default()
 
 with Graph("Basic Architecture", theme=theme):
     Flow([Node("A"), Node("B"), [Node("C"), Node("D"), Node("E")]])
