5652910a8b9ab1c45100761366bbfb0f21c9730b
O'Connor <jsoconno@gmail.com>
2021-01-17 21:33:29 -0500

    updated advert

--- examples/testing.py
+++ examples/testing.py
@@ -1,20 +1,20 @@
 from architectures.core import Graph, Cluster, Group, Node, Edge, Flow
 
 # Import the LightMode theme
-from architectures.themes import LightMode
+from architectures.themes import Default, LightMode
 
 theme = LightMode()
 
 with Graph("My Graph", theme=theme):
-    with Cluster("Cluster A") as cluster_a:
-        a = Node("A")
-        b = Node("B")
-        c = Node("C")
-
-    with Cluster("Cluster B") as cluster_b:
-        d = Node("D")
-        e = Node("E")
-
-    f = Node("F")
-
-    Flow([a, [b, c], cluster_b, f, d, e])
+    with Cluster():
+        Flow(
+            [
+                Node("A"), 
+                [
+                    Node("B"),
+                    Node("C"),
+                    Node("D")
+                ],
+                Node("E")
+            ]
+        )
