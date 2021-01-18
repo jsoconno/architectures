50b0a5ac98f6af2a6646a82ee0643cbd5fbe0b44
O'Connor <jsoconno@gmail.com>
2021-01-18 15:00:03 -0500

    added setup.py file and published packate to pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="architectures",
+    version="0.1.0",
+    author="Justin O'Connor",
+    author_email="jsoconno@gmail.com",
+    description="Tools for creating architecture as code using Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsoconno/architectures",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
