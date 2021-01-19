d170ec589b640ae03a35ca94826888a5c096ec7a
O'Connor <jsoconno@gmail.com>
2021-01-18 21:55:58 -0500

    testing python install

--- scripts/code_coverage.py
+++ scripts/code_coverage.py
@@ -3,4 +3,4 @@
 import os
 
 #os.system("python3 -m pytest --cov=architectures tests --cov-report xml")
-os.system("<(curl -s https://codecov.io/bash) -t b7c724c3-f526-498a-9bc5-7b9df8537d37")
+os.system("bash <(curl -s https://codecov.io/bash) -t TOKEN")
