0b3e429d45854f02d808d49646cad8bca09286e9
O'Connor <jsoconno@gmail.com>
2021-01-19 15:13:04 -0500

    test codecov workflow

--- scripts/code_coverage.py
+++ scripts/code_coverage.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
-import os
+# import os
+# import subprocess as sp
+# import requests
+# res = requests.get('https://stackoverflow.com/questions/26000336')
 
-#os.system("python3 -m pytest --cov=architectures tests --cov-report xml")
-os.system("bash <(curl -s https://codecov.io/bash) -t TOKEN")
+# os.system("python3 -m pytest --cov=architectures tests --cov-report xml")
+#os.system("bash curl -s https://codecov.io/bash -t b7c724c3-f526-498a-9bc5-7b9df8537d37")
+
+# os.system('openssl req -nodes -newkey rsa:2048 -sha256 -keyout mynode.key -out mynode.csr -subj "/C=US/ST=Florida/L=St Petersburg/O=MyCompany/OU=MyOU/CN=mynode/emailAddress=administrators@my.com" -reqexts OPTS -config <(cat /etc/pki/tls/openssl.cnf <(printf "[OPTS]\  basicConstraints = CA:FALSE  subjectAltName = DNS:mynode,DNS:myf5  keyUsage = digitalSignature, keyEncipherment  extendedKeyUsage = serverAuth, clientAuth"))')
+
+# { cat /etc/pki/tls/openssl.cnf;
+#   printf "[OPTS]\  basicConstraints = CA:FALSE  subjectAltName = DNS:mynode,DNS:myf5  keyUsage = digitalSignature, keyEncipherment  extendedKeyUsage = serverAuth, clientAuth";
+# } | openssl req -nodes -newkey rsa:2048 -sha256 -keyout mynode.key -out mynode.csr -subj "/C=US/ST=Florida/L=St Petersburg/O=MyCompany/OU=MyOU/CN=mynode/emailAddress=administrators@my.com" -reqexts OPTS -config -
