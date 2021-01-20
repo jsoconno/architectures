01ddbcc3f2d257da86e5246f6a8ba82a0d473514
O'Connor <jsoconno@gmail.com>
2021-01-20 08:17:05 -0500

    incremented the PyPi package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="architectures",
-    version="0.1.0",
+    version="0.1.1",
     author="Justin O'Connor",
     author_email="jsoconno@gmail.com",
     description="Tools for creating architecture as code using Python.",
