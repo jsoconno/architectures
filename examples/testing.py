1cfba086b77b16d93a9f5d61d24074513f9db6a5
O'Connor <jsoconno@gmail.com>
2021-01-21 11:14:49 -0500

    updated testing.py example

--- examples/testing.py
+++ examples/testing.py
@@ -3,18 +3,13 @@ from architectures.core import Graph, Cluster, Group, Node, Edge, Flow
 # Import the LightMode theme
 from architectures.themes import Default, LightMode
 
-theme = LightMode()
+theme = LightMode(graph_attr_overrides={"rankdir": "TB"})
 
 with Graph("My Graph", theme=theme):
     with Cluster() as cluster_a:
-        with Cluster() as cluster_b:
-            a = Node("A")
-            b = Node("B")
-            c = Node("C")
-
-    with Cluster() as cluster_c:
-        with Cluster() as cluster_d:
-            d = Node("D")
-            e = Node("E")
-
-    Flow([c, e], ltail=cluster_a.name)
+        snooze = Node("Snooze")
+        code = Node("Code")
+        coffee = Node("Coffee")
+    
+    Flow([coffee, code, snooze], constraint="False")
+    Edge(snooze, coffee, style="dotted", constraint="False")
