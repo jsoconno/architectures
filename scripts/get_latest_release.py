d7818b8aad68ebe40a0e035a4e41d5b7b602fa15
O'Connor <jsoconno@gmail.com>
2021-01-25 16:05:56 -0500

    created function to pull any required release data with the ability to parse with regex to make version dynamic based on the release created in github

--- scripts/get_latest_release.py
+++ scripts/get_latest_release.py
@@ -1,13 +1,25 @@
 import os
+import json
+import requests
+import re
 
-def get_latest_release_version(user, repo):
-    command = f"""
-    curl --silent "https://api.github.com/repos/{user}/{repo}/releases/latest" | 
-    grep '"tag_name":' | 
-    sed -E 's/.*"v([^"]+)".*/\1/'   
+def get_release_data(user, repo, field=None, regex_pattern=None, group_number=0):
     """
+    Extract data from the latest release.
+    """
+    response = requests.get(f"https://api.github.com/repos/{user}/{repo}/releases/latest")
+    release_data_dict = json.loads(response.text)
 
-    return os.popen(command).read().strip()
+    if field:
+        field_value = release_data_dict[field]
+        if regex_pattern is None:
+            output = field_value
+        else:
+            output = re.search(regex_pattern, field_value).group(group_number)
+    else:
+        output = release_data_dict
 
-version = get_latest_release_version('jsoconno', 'architectures')
+    return output
+
+version = get_release_data(user="jsoconno", repo="architectures", field="tag_name", regex_pattern="[0-9]*\.[0-9]*\.[0-9]*")
 print(version)
--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
-import os
 import setuptools
+import json
+import requests
+import re
 
-command = r"""
-curl --silent "https://api.github.com/repos/jsoconno/architectures/releases/latest" | 
-grep '"tag_name":' | 
-sed -E 's/.*"v([^"]+)".*/\1/'   
-"""
+def get_release_data(user, repo, field=None, regex_pattern=None, group_number=0):
+    """
+    Extract data from the latest release.
+    """
+    response = requests.get(f"https://api.github.com/repos/{user}/{repo}/releases/latest")
+    release_data_dict = json.loads(response.text)
 
-version = os.popen(command).read().strip()
+    if field:
+        field_value = release_data_dict[field]
+        if regex_pattern is None:
+            output = field_value
+        else:
+            output = re.search(regex_pattern, field_value).group(group_number)
+    else:
+        output = release_data_dict
+
+    return output
+
+version = get_release_data(user="jsoconno", repo="architectures", field="tag_name", regex_pattern="[0-9]*\.[0-9]*\.[0-9]*")
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
