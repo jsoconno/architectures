aeccca8437911900422ad7356891fd50876b2973
O'Connor <jsoconno@gmail.com>
2021-01-25 09:59:35 -0500

    testing wrapping get tag functionality in a function (not working)

--- scripts/get_latest_release.py
+++ scripts/get_latest_release.py
@@ -1,10 +1,13 @@
 import os
 
-command = r"""
-curl --silent "https://api.github.com/repos/jsoconno/architectures/releases/latest" | 
-grep '"tag_name":' | 
-sed -E 's/.*"v([^"]+)".*/\1/'   
-"""
+def get_latest_release_version(user, repo):
+    command = f"""
+    curl --silent "https://api.github.com/repos/{user}/{repo}/releases/latest" | 
+    grep '"tag_name":' | 
+    sed -E 's/.*"v([^"]+)".*/\1/'   
+    """
 
-version = os.popen(command).read().strip()
+    return os.popen(command).read().strip()
+
+version = get_latest_release_version('jsoconno', 'architectures')
 print(version)
