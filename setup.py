1cb09c87cdc7dbb755a454b9d9246c90259107cd
Justin OConnor <jsoconno@gmail.com>
2021-08-09 14:01:36 -0400

    update code to add a manifest file so icons are included along with the distribution

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 import setuptools
-import json
-import requests
-import re
+# import json
+# import requests
+# import re
 
-def get_release_data(user, repo, field=None, regex_pattern=None, group_number=0):
-    """
-    Extract data from the latest release.
-    """
-    response = requests.get(f"https://api.github.com/repos/{user}/{repo}/releases/latest")
-    release_data_dict = json.loads(response.text)
+# def get_release_data(user, repo, field=None, regex_pattern=None, group_number=0):
+#     """
+#     Extract data from the latest release.
+#     """
+#     response = requests.get(f"https://api.github.com/repos/{user}/{repo}/releases/latest")
+#     release_data_dict = json.loads(response.text)
 
-    if field:
-        field_value = release_data_dict[field]
-        if regex_pattern is None:
-            output = field_value
-        else:
-            output = re.search(regex_pattern, field_value).group(group_number)
-    else:
-        output = release_data_dict
+#     if field:
+#         field_value = release_data_dict[field]
+#         if regex_pattern is None:
+#             output = field_value
+#         else:
+#             output = re.search(regex_pattern, field_value).group(group_number)
+#     else:
+#         output = release_data_dict
 
-    return output
+#     return output
 
-version = get_release_data(user="jsoconno", repo="architectures", field="tag_name", regex_pattern="[0-9]*\.[0-9]*\.[0-9]*")
+# version = get_release_data(user="jsoconno", repo="architectures", field="tag_name", regex_pattern="[0-9]*\.[0-9]*\.[0-9]*")
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="architectures",
-    version=version,
+    version='0.2.6',
     author="Justin O'Connor",
     author_email="jsoconno@gmail.com",
     description="Tools for creating architecture as code using Python.",
@@ -36,6 +36,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jsoconno/architectures",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
