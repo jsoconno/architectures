c657a736723fb4e2a51842512e027c52ded48b94
Justin OConnor <jsoconno@gmail.com>
2021-08-09 22:28:35 -0400

    uncommon code in setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 import setuptools
-# import json
-# import requests
-# import re
+import json
+import requests
+import re
 
-# def get_release_data(user, repo, field=None, regex_pattern=None, group_number=0):
-#     """
-#     Extract data from the latest release.
-#     """
-#     response = requests.get(f"https://api.github.com/repos/{user}/{repo}/releases/latest")
-#     release_data_dict = json.loads(response.text)
+def get_release_data(user, repo, field=None, regex_pattern=None, group_number=0):
+    """
+    Extract data from the latest release.
+    """
+    response = requests.get(f"https://api.github.com/repos/{user}/{repo}/releases/latest")
+    release_data_dict = json.loads(response.text)
 
-#     if field:
-#         field_value = release_data_dict[field]
-#         if regex_pattern is None:
-#             output = field_value
-#         else:
-#             output = re.search(regex_pattern, field_value).group(group_number)
-#     else:
-#         output = release_data_dict
+    if field:
+        field_value = release_data_dict[field]
+        if regex_pattern is None:
+            output = field_value
+        else:
+            output = re.search(regex_pattern, field_value).group(group_number)
+    else:
+        output = release_data_dict
 
-#     return output
+    return output
 
-# version = get_release_data(user="jsoconno", repo="architectures", field="tag_name", regex_pattern="[0-9]*\.[0-9]*\.[0-9]*")
+version = get_release_data(user="jsoconno", repo="architectures", field="tag_name", regex_pattern="[0-9]*\.[0-9]*\.[0-9]*")
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
